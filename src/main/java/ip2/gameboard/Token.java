a46bc3c1bb9b1a92c666f51041f733794ed8b336
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2023-01-04 10:24:08 +0100

    Ændret på Player classen så den passer på mvp

---
+++ src/main/java/ip2/gameboard/Token.java
@@ -0,0 +1,54 @@
+package ip2.gameboard;
+
+public class Token {private final Player player; //The player assigned to the token
+    private int position; //The token position
+
+    /**
+     * The constructor for token
+     * @param Player (Player) The player assigned to the token
+     */
+    public Token(Player Player) {
+        this.player = Player;
+        this.position = 0;
+    }
+
+    /**
+     * Moves the position of the token.
+     * @param dice (int) The total dice sum.
+     * @return (boolean) True if the player passes start
+     */
+    public boolean moveToken(int dice) {
+        //If the new position is greater than 24, the player has lapped the board and the position goes back to 0
+        //Else the dice sum is just added to the current position
+        if (position + dice >= 40) {
+            position = position + dice - 40;
+            return true;
+        } else {
+            position += dice;
+            return false;
+        }
+    }
+
+    /**
+     * Sets the token position to a given value
+     * @param newPosition (int) The position the token should be at
+     * @return (boolean) True if the player passes start
+     */
+    public boolean setPosition(int newPosition) {
+        if (newPosition <= position){
+            position = newPosition;
+            return true;
+        } else {
+            position = newPosition;
+            return false;
+        }
+    }
+
+    /**
+     * @return (int) Current position of the token.
+     */
+    public int getPosition() {
+        return position;
+    }
+
+}
