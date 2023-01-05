9899a07f6f4c62ced015f89e418343ad55c9b3b9
Mads Hauberg <mads095h@gmail.com>
2023-01-05 11:50:42 +0100

    Update Deck.java

--- src/main/java/ip2/game/Deck.java
+++ src/main/java/ip2/game/Deck.java
@@ -7,13 +7,14 @@ public class Deck {
 
     private static ArrayList<Cards> cards = new ArrayList<Cards>();
 
-    public static void shuffle() {
-
-        Collections.shuffle(cards);
-
+    public void createCards() {
+        for (int i = 0; i < 36; i++) {
+            Cards newCard = new Cards("chance" + (i + 1));
+            cards.add(newCard);
+        }
     }
 
+    public static void shuffle() {
+        Collections.shuffle(cards);
+    }
 }
-
-
-
