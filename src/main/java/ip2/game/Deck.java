5ae62d97f7d59c08d2cca3c31be471f0ed7212e1
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-01-05 11:48:35 +0100

    Made shuffle method

--- src/main/java/ip2/game/Deck.java
+++ src/main/java/ip2/game/Deck.java
@@ -1,6 +1,19 @@
 package ip2.game;
 
+import java.util.Collections;
+import java.util.ArrayList;
+
 public class Deck {
 
-    private Cards cards[];
+    private static ArrayList<Cards> cards = new ArrayList<Cards>();
+
+    public static void shuffle() {
+
+        Collections.shuffle(cards);
+
+    }
+
 }
+
+
+
