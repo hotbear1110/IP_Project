f8afc0104c3828e71c23c1dc501f00a2abb9fe34
Mads Hauberg <mads095h@gmail.com>
2023-01-07 13:26:28 +0100

    Started adding some i18n stuff, prob has to be moved out of this class to a helper class at some point

--- src/main/java/Model/ChanceCards/Deck.java
+++ src/main/java/Model/ChanceCards/Deck.java
@@ -1,12 +1,36 @@
 package Model.ChanceCards;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Deck {
 
     private static ArrayList<Cards> cards = new ArrayList<Cards>();
 
+    File file = new File("../../../resources");
+    URL[] urls;
+
+    {
+        try {
+            urls = new URL[]{file.toURI().toURL()};
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    ClassLoader loader = new URLClassLoader(urls);
+    Locale bLocale = new Locale.Builder().setLanguage("da").setRegion("DK").build();
+    ResourceBundle i18n = ResourceBundle.getBundle("MessageBundle",  bLocale, loader);
+
+    /**
+     * Vi skal nok have flyttet alt om i18n til en hjælper klasse på et tidspunkt
+     */
     public void createCards() {
         for (int i = 0; i < 36; i++) {
             Cards newCard = new Cards("chance" + (i + 1));
