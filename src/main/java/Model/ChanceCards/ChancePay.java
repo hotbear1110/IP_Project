fc083b21e4aa6dcc9d04d013a7b3581b515e907c
Mads Hauberg <mads095h@gmail.com>
2023-01-07 10:45:59 +0100

    Made a constructor for chancepay

--- src/main/java/Model/ChanceCards/ChancePay.java
+++ src/main/java/Model/ChanceCards/ChancePay.java
@@ -1,4 +1,25 @@
 package Model.ChanceCards;
 
 public class ChancePay {
+
+    private String name;
+
+    private String description;
+
+    private boolean regular;
+
+    private boolean perPlayer;
+
+    private boolean propertyBonus;
+
+    public ChancePay(String Name, String Description, boolean Regular, boolean PerPlayer, boolean PropertyBonus) {
+        name = Name;
+        Description = description;
+        Regular = regular;
+        PerPlayer = perPlayer;
+        PropertyBonus = propertyBonus;
+    }
+
+
+
 }
