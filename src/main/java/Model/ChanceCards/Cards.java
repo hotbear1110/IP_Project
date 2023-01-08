1d087c588fb6d120c765f83c64b2200b8e432ebd
Mads Hauberg <mads095h@gmail.com>
2023-01-09 00:36:11 +0100

    Made a getType method

--- src/main/java/Model/ChanceCards/Cards.java
+++ src/main/java/Model/ChanceCards/Cards.java
@@ -1,11 +1,19 @@
 package Model.ChanceCards;
 
+import Model.Player;
+
 public class Cards {
 
-private String name;
+    private String name;
+    private String type;
 
-    public Cards(String Name) {
+    public Cards(String Name, String Type) {
         name = Name;
+        type = Type;
+    }
+
+    public String getType() {
+        return this.type;
     }
 
 }
