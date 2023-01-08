8cf9944a9567efd8c29c58d2c2182f9c349c6db9
Mads Hauberg <mads095h@gmail.com>
2023-01-08 13:08:36 +0100

    Fixed minor issues

--- src/main/java/Model/ChanceCards/MovePlayer.java
+++ src/main/java/Model/ChanceCards/MovePlayer.java
@@ -1,16 +1,25 @@
 package Model.ChanceCards;
 
 import Model.Player;
+
 public class MovePlayer {
 
+    private static String name;
+
+    private static String type;
+
+    private static String description;
 
     private static int fields;
-    public MovePlayer(int Fields){
+    public MovePlayer(String Name, String Type, String Description, int Fields){
+        name = Name;
+        type = Type;
+        description = Description;
         fields = Fields;
     }
 
-    public static void move(Player player)
-        Player.getCurrentPlayerPosition();
-        Player.setCurrentPlayerPosition(fields)
+    public static void move(Player player) {
+        Player.moveCurrentPlayerPosition(fields);
+    }
 
 }
