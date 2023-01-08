ee57cdbaa2f5fe7276f909621f46a1beeb13c833
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-01-08 13:03:16 +0100

    Move player metode

--- src/main/java/Model/ChanceCards/MovePlayer.java
+++ src/main/java/Model/ChanceCards/MovePlayer.java
@@ -1,4 +1,16 @@
 package Model.ChanceCards;
 
+import Model.Player;
 public class MovePlayer {
+
+
+    private static int fields;
+    public MovePlayer(int Fields){
+        fields = Fields;
+    }
+
+    public static void move(Player player)
+        Player.getCurrentPlayerPosition();
+        Player.setCurrentPlayerPosition(fields)
+
 }
