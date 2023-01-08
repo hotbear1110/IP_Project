8fc9a1d41b2f2580581a4621269aa73a086d916b
Mads Hauberg <mads095h@gmail.com>
2023-01-08 13:22:13 +0100

    Made the jail card

--- src/main/java/Model/ChanceCards/JailCard.java
+++ src/main/java/Model/ChanceCards/JailCard.java
@@ -1,4 +1,14 @@
 package Model.ChanceCards;
 
 public class JailCard {
+    private static String name;
+
+    private static String type;
+
+    private static String description;
+    public JailCard(String Name, String Type, String Description){
+        name = Name;
+        type = Type;
+        description = Description;
+    }
 }
