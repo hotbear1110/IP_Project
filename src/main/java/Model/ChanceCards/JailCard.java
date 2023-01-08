3d834dfd8540eb0e614d6423dacfa178be4ea96d
Mads Hauberg <mads095h@gmail.com>
2023-01-09 00:37:42 +0100

    Made the class extend Cards

--- src/main/java/Model/ChanceCards/JailCard.java
+++ src/main/java/Model/ChanceCards/JailCard.java
@@ -1,14 +1,9 @@
 package Model.ChanceCards;
 
-public class JailCard {
-    private static String name;
-
-    private static String type;
-
+public class JailCard extends Cards{
     private static String description;
     public JailCard(String Name, String Type, String Description){
-        name = Name;
-        type = Type;
+        super(Name, Type);
         description = Description;
     }
 }
