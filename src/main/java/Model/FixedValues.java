3476519e3d1073d72f014d6f499b3bb227a0e66d
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2023-01-08 10:49:38 +0100

    Lavet Fixed Values

---
+++ src/main/java/Model/FixedValues.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class FixedValues {
+    public int StartMoney = 30000;
+    public int PassedStart = 4000;
+    public int NumOfSqaures = 40;
+    public int MinPlayers = 3;
+    public int MaxPlayers = 6;
+    public int NumDice = 2;
+    public int TaxPrice = 10;
+
+}
