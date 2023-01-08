23b557d8fe5cbba28f58ad95a23d198b28cee0c7
Mads Hauberg <mads095h@gmail.com>
2023-01-09 00:38:09 +0100

    Made the class extend Cards and removed static

--- src/main/java/Model/ChanceCards/MovePlayer.java
+++ src/main/java/Model/ChanceCards/MovePlayer.java
@@ -2,31 +2,26 @@ package Model.ChanceCards;
 
 import Model.Player;
 
-public class MovePlayer {
+public class MovePlayer extends Cards {
 
-    private static String name;
+    private String description;
 
-    private static String type;
-
-    private static String description;
-
-    private static int fields;
+    private int fields;
     public MovePlayer(String Name, String Type, String Description, int Fields){
-        name = Name;
-        type = Type;
+        super(Name, Type);
         description = Description;
         fields = Fields;
     }
 
-    public static void move(Player player) {
+    public void move(Player player) {
         Player.moveCurrentPlayerPosition(fields);
     }
 
-    public static void specific(Player player) {
+    public void specific(Player player) {
         Player.setCurrentPlayerPosition(fields);
     }
 
-    public static void jail(Player player) {
+    public void jail(Player player) {
         //Vi skal lige finde ud af en måde spilleren ikke får penge på
         Player.setCurrentPlayerPosition(fields);
     }
