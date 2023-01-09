9fdddbef50f530853e11831c714cce0acc2cdbee
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-01-09 15:41:15 +0100

    Fixed the commented methods

--- src/main/java/Model/ChanceCards/ChanceReceive.java
+++ src/main/java/Model/ChanceCards/ChanceReceive.java
@@ -14,27 +14,24 @@ public class ChanceReceive extends Cards {
         player.setPlayerBalance(amount);
     }
 
-    /*public void steal(Player player, int amountOfPlayers) {
+    public void steal(Player player, Player[] players) {
+        int amountOfPlayers = players.length;
         player.setPlayerBalance(amount*amountOfPlayers+200);
-        Player[] players = Game.getPlayers();
         for (Player player1: players){
-            player1.setPlayersBalance(amount);
+            player1.setPlayerBalance(amount);
         }
-    }*/
+    }
 
     /**
      * Metode der checker om playeren har værdier for mere end 15000kr. Hvis ikke, så modtager de 40000kr
      */
 
-    /*public void  rarecieve(Player player){
-        int balance = Player.getPlayerBalance();
+    public void  rarecieve(Player player){
+        int balance = player.getPlayerBalance();
 
-        if (balance > 15000) {
-            return;
-        }
-        else {
+        if (balance <= 15000) {
             player.setPlayerBalance(40000);
         }
-    }*/
+    }
 
 }
