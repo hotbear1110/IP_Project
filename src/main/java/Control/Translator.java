0393f7856c96312a1397ac4c4530b92533c08305
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-01-09 13:03:57 +0100

    Made a translator class for i18n

---
+++ src/main/java/Control/Translator.java
@@ -0,0 +1,31 @@
+package Control;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Translator {
+    private File file = new File("../../resources");
+    private static URL[] urls;
+
+    {
+        try {
+            urls = new URL[]{file.toURI().toURL()};
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static ClassLoader loader = new URLClassLoader(urls);
+    private static Locale bLocale = new Locale.Builder().setLanguage("da").setRegion("DK").build();
+    private static ResourceBundle i18n = ResourceBundle.getBundle("MessageBundle",  bLocale, loader);
+
+    public static String getString(String name) {
+        String response = i18n.getString(name);
+        return response;
+    }
+
+}
