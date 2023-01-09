0d8f51d4ce668d626489bfd8748b4ceb798a5926
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2023-01-09 12:22:55 +0100

    leje for rødovrevej og hvidovrevej

--- src/main/java/Model/PricesFactory.java
+++ src/main/java/Model/PricesFactory.java
@@ -3,9 +3,10 @@ package Model;
 public class PricesFactory {
     public static int[][] rentTable(){
         int[][] rentTable = new int[][]{
-                //leje
-                {},
-                {},
+                //Rødovrevej, leje, dobbelt leje, 1 hus, 2 hus, 3 hus, 4 hus, hotel
+                {50, 100, 250, 750, 2250, 4000, 6000},
+                //Hvidovrevej
+                {50, 100, 250, 750, 2250, 4000, 6000},
         };
         return rentTable;
     }
