a4c020b9e03a82a0b30c2f65fd65d6d4ad848f4f
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-01-11 10:31:31 +0100

    Metro extends board

--- src/main/java/Model/Squares/Metro.java
+++ src/main/java/Model/Squares/Metro.java
@@ -2,4 +2,7 @@ package Model.Squares;
 
 public class Metro extends Square {
 
+    public Metro(String name, String subText, String description){
+        super(name,subText, description);
+    }
 }
