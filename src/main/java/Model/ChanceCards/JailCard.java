5a3dfa7e06d0b76117386ba05c9277c67b26ce1d
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-01-16 13:11:06 +0100

    Updated jailcard to be up to date with development

--- src/main/java/Model/ChanceCards/JailCard.java
+++ src/main/java/Model/ChanceCards/JailCard.java
@@ -1,7 +1,13 @@
 package Model.ChanceCards;
 
+import Model.Player;
+
 public class JailCard extends Cards{
     public JailCard(String Name, String Type, String Description){
         super(Name, Type, Description);
+    }
+
+    public void giveJailCard(Player player) {
+        player.giveJailCard();
     }
 }
