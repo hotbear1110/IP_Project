58370daa76c17ea30d24eedbf684f7d04942f809
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-01-16 13:10:04 +0100

    Made a landsonchance method

--- src/main/java/Control/ChanceControl.java
+++ src/main/java/Control/ChanceControl.java
@@ -1,8 +1,25 @@
 package Control;
 
+import Model.Player;
+
 public class ChanceControl {
     private GameControl gameControl;
     public ChanceControl(GameControl gameControl){
         this.gameControl = gameControl;
+    }
+
+    public void landOnChance(Player player) {
+        Player[] players = gameControl.getGame().getPlayers();
+        String description = gameControl.getGame().getBoard().getNextCard();
+
+        gameControl.getUI().setChanceCard(description);
+
+        gameControl.getGame().getBoard().drawCard(player, players);
+
+        gameControl.getUI().getChanceCard(description);
+
+        String nextDescription = gameControl.getGame().getBoard().getNextCard();
+
+        gameControl.getUI().setChanceCard(nextDescription);
     }
 }
