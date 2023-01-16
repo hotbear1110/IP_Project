8a74480e0a16095ebc5a5f8bb4c2ff6731344f3e
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-01-16 13:12:22 +0100

    Updated moveplayer

--- src/main/java/Model/ChanceCards/MovePlayer.java
+++ src/main/java/Model/ChanceCards/MovePlayer.java
@@ -12,17 +12,22 @@ public class MovePlayer extends Cards {
         fields = Fields;
     }
 
-    /*public void move(Player player) {
-        Player.moveCurrentPlayerPosition(fields);
+    public void move(Player player) {
+        player.movePlayerPosition(fields);
     }
 
     public void specific(Player player) {
-        Player.setCurrentPlayerPosition(fields);
+        player.movePlayerPosition(fields);
     }
 
     public void jail(Player player) {
         //Vi skal lige finde ud af en måde spilleren ikke får penge på
-        Player.setCurrentPlayerPosition(fields);
-    }*/
+        player.movePlayerPosition(fields);
+    }
+
+    public void moveToNext(Player player) {
+        //Vi skal lige finde ud af en måde at se hvilet redderi der er det næste
+        player.movePlayerPosition(fields);
+    }
 
 }
