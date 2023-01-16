eb7a86f066194ee15d257097f1c4aa74539c5227
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-01-16 11:35:05 +0100

    UPDATED I18N
    Updated the danish and english translations with getstring

--- src/main/java/Control/ControlMenus.java
+++ src/main/java/Control/ControlMenus.java
@@ -4,14 +4,14 @@ public class ControlMenus {
 
     public static String[] diceMenu = new String[]{Translator.getString("ROLL_DICE")};
 
-    public static String[] startMenu = new String[]{"Rul med terningerne", "Køb/Sælg", "Pantsæt/Ophæv pantsætning"};
+    public static String[] startMenu = new String[]{Translator.getString("START_THE_TURN"), Translator.getString("BUY_SELL"), Translator.getString("MORTGAGE")};
 
-    public static String[] buySellMenu = new String[]{"Køb/Sælg opgraderinger", "Sælg grunde", "Tilbage"};
+    public static String[] buySellMenu = new String[]{Translator.getString("BUY_SELL_UPGRADES"), Translator.getString("SELL_PROPERTY"), Translator.getString("BACK")};
 
-    public static String[] upgradeMenu = new String[]{"Køb opgraderinger", "Sælg opgraderinger", "Tilbage"};
+    public static String[] upgradeMenu = new String[]{Translator.getString("BUY_UPGRADES"), Translator.getString("SELL_UPGRADES"), Translator.getString("BACK")};
 
-    public static String[] buyUpgradeMenu = new String[]{"Køb hus", "Køb hotel", "Tilbage"};
+    public static String[] buyUpgradeMenu = new String[]{Translator.getString("BUY_HOUSE"), Translator.getString("BUY_HOTEL"), Translator.getString("BACK")};
 
-    public static String[] sellHotelUpgradesMenu = new String[]{"Sælg hotel", "Tilbage"};
-    public static String[] sellHouseUpgradesMenu = new String[]{"Sælg et hus", "Tilbage"};
+    public static String[] sellHotelUpgradesMenu = new String[]{Translator.getString("SELL_HOTEL"), Translator.getString("BACK")};
+    public static String[] sellHouseUpgradesMenu = new String[]{Translator.getString("SELL_A_HOUSE"), Translator.getString("BACK")};
 }
