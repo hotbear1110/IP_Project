6ea78e6095758fe57ebb5f48587a958a76ba1464
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-01-17 11:45:13 +0100

    Updaten i18n

--- src/main/java/Control/ControlMenus.java
+++ src/main/java/Control/ControlMenus.java
@@ -8,11 +8,11 @@ public class ControlMenus {
 
     public static String[] startMenu = new String[]{Translator.getString("START_TURN"), Translator.getString("SEE_OWNED_PROP")};
 
-    public static String[] buySellMenu = new String[]{Translator.getString("BUY_SELL_UPGRADES"), Translator.getString("PAWN_REMOVE_PAWN "), Translator.getString("BACK")};
+    public static String[] buySellMenu = new String[]{Translator.getString("BUY_SELL_UPGRADES"), Translator.getString("PAWN_REMOVE_PAWN"), Translator.getString("BACK")};
 
     public static String[] upgradeMenu = new String[]{Translator.getString("BUY_UPGRADES"), Translator.getString("SELL_UPGRADES"), Translator.getString("BACK")};
 
-    public static String[] buyUpgradeMenu = new String[]{Translator.getString("BUY_HOUSE"), Translator.getString("BUY HOTEL"), Translator.getString("BACK")};
+    public static String[] buyUpgradeMenu = new String[]{Translator.getString("BUY_HOUSE"), Translator.getString("BUY_HOTEL"), Translator.getString("BACK")};
 
     public static String[] sellHotelUpgradesMenu = new String[]{Translator.getString("SELL_HOTEL"), Translator.getString("BACK")};
     public static String[] sellHouseUpgradesMenu = new String[]{Translator.getString("SELL_HOUSE"), Translator.getString("BACK")};
