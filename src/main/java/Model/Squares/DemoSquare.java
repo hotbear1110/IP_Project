865993226a91f04031fabd3651405d09a8c8973c
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2023-01-17 11:05:08 +0100

    corrected description in boardFactory

--- src/main/java/Model/Squares/DemoSquare.java
+++
@@ -1,8 +0,0 @@
-package Model.Squares;
-
-public class DemoSquare extends Square {
-
-    public DemoSquare(String name, String subText, String description){
-        super(name, subText, description);
-    }
-}
