352b376093a0c6e24c549dbfaeac3b20690fe563
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2023-01-17 16:02:15 +0100

    Userstory K21,K22 Accept-test 9

---
+++ src/test/java/PawnAPropertyAndRebuy.java
@@ -0,0 +1,7 @@
+import Control.GameControl;
+
+public class PawnAPropertyAndRebuy {
+    public static void main(String[] args ){
+        GameControl gameControl = new GameControl("K21/K22");
+    }
+}
--- src/test/java/UpgradeAndSellUpgradeTest.java
+++ src/test/java/UpgradeAndSellUpgradeTest.java
@@ -3,7 +3,6 @@ import Model.Board;
 
 public class UpgradeAndSellUpgradeTest {
     public static void main(String[] args ){
-        Board board = new Board();
-        GameControl gameControl = new GameControl(board, "K18/K19/K20");
+        GameControl gameControl = new GameControl("K18/K19/K20");
     }
 }
