3cc1e3138b64cc25822ea8719a5c7a95a4f00cb7
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2023-01-17 17:54:56 +0100

    Lavede acceptest for K7/K17 DoubleRentTest

---
+++ src/test/java/DoubleRentTest.java
@@ -0,0 +1,8 @@
+import Control.GameControl;
+import Model.Board;
+
+public class DoubleRentTest {public static void main(String[] args ){
+    Board board = new Board();
+    GameControl gameControl = new GameControl(board,"K7/K17");
+}
+}
